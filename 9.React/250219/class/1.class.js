018198bbbe04d215f1391bc57e1e394e866e3280
imbhj <wnqudgus5565@gmail.com>
2025-02-20 19:18:07 +0900

    fix: 250220 수업자료 업로드

--- 9.React/250219/class/1.class.js
+++ 9.React/250219/class/1.class.js
@@ -1,15 +1,20 @@
 class Cat {
    constructor(props) {
-    // 이름이라는 추상적인 데이터를 정의하고 싶다.
-    // cat1.name = props;
-    // cat2.name = props;
-    this.name = props;
+    this.name = props.name;
+    this.meowwww = props.meowwww
    } 
+   meow () {
+      return this.meowwww + this.name
+   }
 }
 
-const cat1 = new Cat("나비");
-const cat2 = new Cat("고앵이");
-console.log(cat2);
+let cat1 = new Cat({name: "나비", meowwww: "미야아아아옹"});
+const cat2 = new Cat({name: "고앵이", meowwww: "월월"});
+// const cat3 = cat1.meow.bind(cat2);
+cat1.meow = cat1.meow.bind(cat2);
+console.log(cat1.meow());
+
+
 
 // 추상적인 존재를 누가 실체화 시켰느냐?에 따라서 this는 달라진다.
 // 즉, 호출 또는 실행되었을 때!!!! this가 결정된다가 이 뜻임.
