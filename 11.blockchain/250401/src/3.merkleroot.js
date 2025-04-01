093d7f563370b45cf8115617aa43642da86a2e54
mls <cjmadlife5930@gmail.com>
2025-04-01 17:03:37 +0900

    병합

--- 11.blockchain/250401/src/3.merkleroot.js
+++
@@ -1,9 +0,0 @@
-const merkle = require("merkle")
-
-// 하버드 1000명 성적표가 담겼다고 가정
-const txData = ["4.5", "4.0", "4.1", "4.2", "4.4", "4.5"]
-
-const merkleTree = merkle("sha256").sync(txData)
-const Root = merkleTree.root();
-console.log(Root);
-// BDB04D99394BB02D966D58F8B7856991444F27CB50C9B544188CCA4467611FFD
