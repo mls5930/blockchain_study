e0a7f932e6a609fc79ed80ef234035db2c6f65c4
imbhj <wnqudgus5565@gmail.com>
2025-04-08 12:04:59 +0900

    250408 수업자료 업로드

--- 13.blockchain/250408/src/core/_test/chain.test.ts
+++ 13.blockchain/250408/src/core/_test/chain.test.ts
@@ -45,7 +45,6 @@ describe("🧪 난이도 조정 흐름 테스트 - 단계별", () => {
   it("4단계. 주기만큼 블록을 추가하고 난 후, 다음 블록을 생성한다", () => {
     for (let i = 1; i <= DIFFICULTY_ADJUSTMENT_INTERVAL + 1; i++) {
       let adjustment: Block;
-      // 10
       const prev = chain.latestBlock()
       
       if (i > DIFFICULTY_ADJUSTMENT_INTERVAL) {
@@ -62,7 +61,7 @@ describe("🧪 난이도 조정 흐름 테스트 - 단계별", () => {
   it("5단계. 난이도가 조정되는지 확인한다", () => {
     for (let i = 1; i <= DIFFICULTY_ADJUSTMENT_INTERVAL + 1; i++) {
       let adjustment: Block;
-      const prev = chain.latestBlock()
+      const prev = chain.latestBlock();
       
       if (i > DIFFICULTY_ADJUSTMENT_INTERVAL) {
         const index = i - DIFFICULTY_ADJUSTMENT_INTERVAL;
@@ -77,6 +76,52 @@ describe("🧪 난이도 조정 흐름 테스트 - 단계별", () => {
     const prevBlock = chain.latestBlock();
     
     const adjustment = chain.get()[chain.length() - DIFFICULTY_ADJUSTMENT_INTERVAL];
+    expect(adjustment.difficulty).not.toBe(prevBlock.difficulty);
+  });
+
+  it("상대방이 더 긴 체인이면 교체되는가?", ()=> {
+    /*
+      가정
+
+      체인끼리의 비교
+
+      1. 내 체인
+      2. 상대방 체인
+
+      상대방이 더 긴 체인이어야 하니까 => 상대방 체인에 블록을 2개 정도 생성해보자.
+    */
+
+    const myChain = new Chain();
+    const otherChain = new Chain();
+    // 상대방 체인 생성 + 블록 추가
+    const b1 = Block.generateBlock(otherChain.latestBlock(), ["tx01"], GENESIS);
+    otherChain.addToChain(b1);
+    const b2 = Block.generateBlock(otherChain.latestBlock(), ["tx02"], b1);
+    otherChain.addToChain(b2);
+
+    // 교체 시도
+    const result = myChain.replaceChain(otherChain.get());
+
+    // 기대 결과 확인
+    expect(result.isError).toBe(false) // 교체 성공
+    expect(myChain.length()).toBe(3);
+    // 
+    expect(myChain.latestBlock().data[0]).toEqual("tx02")
+  })
+
+  it("getAdjustmentBlock => 난이도가 조정되는지 확인한다", () => {
+    const newChain = new Chain();
+    for (let i = 1; i <= DIFFICULTY_ADJUSTMENT_INTERVAL + 1; i++) {
+      const latestBlock = newChain.latestBlock();
+      const adjustment = newChain.getAdjustmentBlock();
+      const data = ["tx01"];
+      const newBlock = Block.generateBlock(latestBlock, data, adjustment);
+      newChain.addToChain(newBlock);
+    }
+    
+    const prevBlock = newChain.latestBlock();
+    
+    const adjustment = newChain.get()[newChain.length() - DIFFICULTY_ADJUSTMENT_INTERVAL];
     expect(adjustment.difficulty).not.toBe(prevBlock.difficulty);
   });
 });
