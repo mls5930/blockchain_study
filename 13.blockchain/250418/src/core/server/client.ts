c47f374604261d4bfb78e1b6f32ffac6fe490066
imbhj <wnqudgus5565@gmail.com>
2025-04-18 09:27:33 +0900

    fix: 250418 수업자료 업로드

---
+++ 13.blockchain/250418/src/core/server/client.ts
@@ -0,0 +1,73 @@
+// 경로: 250417/src/core/server/client.ts
+import net from "net"
+import Chain from "@core/chain/chain";
+import { IMessage, MessageType } from "./interface/message.interface";
+import Block from "@core/block/block";
+
+export class Client {
+    constructor(private readonly chain: Chain) {}
+
+    connect(host: string, port: number, type: MessageType, payload?: string[]): void {
+        const client = new net.Socket();
+        client.connect(port, host, () => {
+            const message = this.message(type, payload);
+            client.write(JSON.stringify(message))
+        })
+
+        client.on("data", (data) => {
+            // { type, payload? }
+            console.log("data", data);
+            
+            const message: IMessage = JSON.parse(data.toString().trim());
+            // node2가 뭘 보내왔나?
+            // p2p.ts => MessageType.allBlock
+            switch (message.type) {
+                case MessageType.addBlock:
+                    this.chain.replaceChain(message.payload as Block[])
+                    break;
+                case MessageType.latestBlock:
+                    console.log("[Latest Block]", message.payload);
+                    break;
+                case MessageType.allBlock:
+                    console.log("[All Block]", this.chain.get());
+                    break;
+            }
+        })
+
+        client.on("close", () => {
+            console.log("[Client] 즉, 터미널 종료함!");  
+        })
+    }
+    
+    private addBlock(payload: string[]): Block[] {
+        const block = Block.generateBlock(
+            this.chain.latestBlock(), 
+            payload, 
+            this.chain.getAdjustmentBlock()
+        );
+        this.chain.addToChain(block);
+        return this.chain.get();
+    }
+
+    private message(type: MessageType, payload: string[]): IMessage {
+        switch (type) {
+            case MessageType.addBlock:
+                return {
+                    type,
+                    payload: this.addBlock(payload)
+                }
+            case MessageType.allBlock:
+                return { 
+                    type, 
+                    payload: this.chain.get()
+                }
+            case MessageType.latestBlock:
+                return {
+                    type,
+                    payload: this.chain.latestBlock()
+                }
+            default:
+                break;
+        }
+    }
+}
---
+++ 13.blockchain/250418/src/core/server/p2p.ts
@@ -0,0 +1,63 @@
+// 경로: 250417/src/core/server/p2p.ts
+
+import net, { Socket } from "net";
+import { IP2P } from "./interface/p2p.interface";
+import { IMessage, MessageType } from "./interface/message.interface";
+import Chain from "@core/chain/chain";
+import Block from "@core/block/block";
+
+export class P2P implements IP2P {
+    sockets: Socket[] = [];
+
+    constructor(private readonly chain: Chain) {}
+
+    listen(port: number): void {
+        const server = net.createServer((socket) => {
+            this.initializeSocket(socket);
+        })
+
+        server.listen(port, () => {
+            console.log(`P2P 서버 실행 중 port: ${port}`);
+        })
+    }
+
+    // 클라이언트 메시지를 수신하고 타입에 따라 처리하는 로직 등록
+    private initializeSocket(socket: Socket): void {
+        this.sockets.push(socket);
+        socket.on('data', (data) => {
+            try{
+                const message: IMessage =  JSON.parse(data.toString().trim());
+                console.log("message", message);
+                
+                switch (message.type) {
+                    case MessageType.addBlock:
+                        this.chain.replaceChain(message.payload as Block[])
+                        this.sendMessage(socket, MessageType.allBlock);
+                        break;
+                    case MessageType.allBlock:
+                        // 1. socket, 2. 메세지 타입, 3. 데이터
+                        this.sendMessage(socket, MessageType.allBlock, this.chain.get());
+                        break;
+                        case MessageType.latestBlock:
+                        this.sendMessage(socket, MessageType.latestBlock, this.chain.latestBlock());
+                        break;
+                    default:
+                        break;
+                }
+            } catch (error) {
+                console.log(`[!] 메세지 처리 오류! ${error.message}`);
+            }
+        })
+        socket.on("end", () => {
+            const index = this.sockets.indexOf(socket);
+            if (index !== -1) this.sockets.splice(index, 1);
+            console.log(`[-] 소켓 연결 종료 ${socket.remoteAddress}: ${socket.remotePort}`);   
+        })
+    }
+
+    private sendMessage(socket: Socket, type: MessageType, payload?: Block | Block[]) : void{
+        const message: IMessage = { type, payload };
+        socket.write(JSON.stringify(message));
+    }
+}
+
